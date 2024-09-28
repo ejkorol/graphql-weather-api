268028aecac20eaac63055eb8c1df5496262079a
Jason Korol <ejkorol@gmail.com>
2024-09-28 00:06:19 -0400

    creates class and new axios instance for handling interfacing with the openmeteo api

---
+++ src/lib/openMeteoApi.ts
@@ -0,0 +1,45 @@
+import axios, { AxiosInstance } from "axios";
+
+/**
+ * Axios instance for interacting with the Open Meteo API.
+ * Provides methods to fetch weather data based on latitude and longitude.
+ *
+ * @class OpenMeteoApi
+ * */
+class OpenMeteoApi {
+  private apiClient: AxiosInstance;
+
+  constructor() {
+    this.apiClient = axios.create({
+      baseURL: process.env.OPEN_METEO_API,
+    });
+  }
+
+  /**
+   * Fetches weather data from the Open Meteo API for a given location.
+   *
+   * @async
+   * @param {number} latitude - The latitude of the location for which to fetch weather data.
+   * @param {number} longitude - The longitude of the location for which to fetch weather data.
+   * @returns {Promise<any>} The weather data retrieved from the Open Meteo API.
+   * @throws {Error} If there is an issue fetching the data from the API.
+   */
+  async fetchFromApi(latitude: number, longitude: number): Promise<any> {
+    try {
+      const ROUTE = `/forecast`;
+      const res = await this.apiClient.get(ROUTE, {
+        params: {
+          latitude,
+          longitude,
+          daily:
+            "temperature_2m_max,temperature_2m_min,weather_code,sunrise,sunset",
+        },
+      });
+      return res.data;
+    } catch (e) {
+      throw new Error(`Error fetching from Open Meteo: ${e}`);
+    }
+  }
+}
+
+export default new OpenMeteoApi();
